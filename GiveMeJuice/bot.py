0fa9598495f38821a4fe3e40a12d18ea527d4271
huewilliams <37013834+huewilliams@users.noreply.github.com>
2018-07-27 11:47:20 +0900

    챗봇연습-GiveMeJuice_bot.ver1.5(선택 메뉴 가격 출력,주문 여부 확인)

--- GiveMeJuice/bot.py
+++ GiveMeJuice/bot.py
@@ -12,8 +12,22 @@ class Bot(BaseBot):
 
         if content.startswith('/start'):
             self.send_welcome_message(event)
+
         elif content == '메뉴보기':
             self.send_menu(event)
+
+        elif content.startswith('/show'):
+            _, name = content.split()
+            self.send_show(name, event)
+
+        elif content.startswith('/order_confirm'):
+            _, name = content.split()
+            self.send_order_confirm(name, event)
+
+        elif content.startswith('/order'):
+            _, name = content.split()
+            self.send_order(name, event)
+
 
     # 봇을 시작했을 때 기능을 알려주는 함수
     def send_welcome_message(self, event):
@@ -35,3 +49,29 @@ class Bot(BaseBot):
             message.add_postback_button(name, '/show {}'.format(name))
 
         self.send_message(message)
+
+    # 선택한 메뉴의 가격을 보여주는 함수
+    def send_show(self, name ,event):
+        menu = self.get_project_data()['menu']
+        selected_menu = menu[name]
+        text = '{name}는 {description}\n 가격은 {price}원 입니다.'.format(name=name, **selected_menu)
+        message = Message(event).set_text(text)\
+                                .add_quick_reply('{} 주문 '.format(name), '/order {}'.format(name))\
+                                .add_quick_reply('메뉴보기')
+        self.send_message(message)
+
+    # 주문할 지 여부를 물어보는 함수
+    def send_order_confirm(self, name, event):
+        message = Message(event).set_text('{}를 주문하시겠습니까?'.format(name))\
+                                .add_quick_reply('예','/order {}'.format(name))\
+                                .add_quick_reply('취소','메뉴보기')
+        self.send_message(message)
+
+    #
+    def send_order(self, name, event, quantity=1):
+        self.send_message('{}를 {}잔 주문했습니다. 음료가 준비되면 알려드리겠습니다.'.format(name, quantity))
+
+        chat_id = self.get_project_data().get('chat_id')
+        order_message = Message(event).set_text('{} {}잔 주문이 들어왔습니다.'.format(name, quantity))\
+                                     .add_quick_reply('완료','/done {} {}'.format(event['sender']['id'], name))
+        self.send_message(order_message, chat_id=chat_id)
