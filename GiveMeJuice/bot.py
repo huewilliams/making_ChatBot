9ac7ff90cc9e2f454fee273a27c3fff6e07fd731
huewilliams <37013834+huewilliams@users.noreply.github.com>
2018-07-30 10:55:49 +0900

    챗봇연습-GiveMeJuice_bot.ver3(고객평가,직원평가확인)

--- GiveMeJuice/bot.py
+++ GiveMeJuice/bot.py
@@ -15,16 +15,20 @@ class Bot(BaseBot):
                 self.send_chatroom_welcome_message(event)
             return
 
+        # 챗봇 시작할 때
         if content.startswith('/start'):
             self.send_welcome_message(event)
 
+        # 사용자가 메뉴보기 요청할 때
         elif content == '메뉴보기':
             self.send_menu(event)
 
+        # 메뉴 선택했을 때 (메뉴 정보 보여주기)
         elif content.startswith('/show'):
             _, name = content.split()
             self.send_show(name, event)
 
+        # 주문하기 여부
         elif content.startswith('/order_confirm'):
             _, name = content.split()
             self.send_order_confirm(name, event)
@@ -33,6 +37,19 @@ class Bot(BaseBot):
             _, name = content.split()
             self.send_order(name, event)
 
+        # 직원이 주문 완료를 눌렀을 때
+        elif content.startswith('/done'):
+            self.send_drink_done(content, event)
+
+        # 고객이 평가하기를 눌렀을 때
+        elif content == '/feedback':
+            self.send_feedback_request()
+
+        else:
+            data = self.get_user_data()
+            wait_feedback = data.get('wait_feedback')
+            if wait_feedback:
+                self.send_feedback(content, event)
 
     # 봇을 시작했을 때 기능을 알려주는 함수
     def send_welcome_message(self, event):
@@ -72,7 +89,7 @@ class Bot(BaseBot):
                                 .add_quick_reply('취소', '메뉴보기')
         self.send_message(message)
 
-    #
+    # 사용자에게 주문 정보, 직원에게 주문 등록
     def send_order(self, name, event, quantity=1):
         self.send_message('{}를 {}잔 주문했습니다. 음료가 준비되면 알려드리겠습니다.'.format(name, quantity))
 
@@ -89,7 +106,35 @@ class Bot(BaseBot):
 
     def remember_chatroom(self, event):
         chat_id = event.get('chat_id')
+        # 현재 사용자의 chat_id를 받아옴
         data = self.get_project_data()
         data['chat_id'] = chat_id
         self.set_project_data(data)
+        # bothub studio 프로퍼티에 아이디 설정
+
+    def send_drink_done(self, content, event):
+        _, sender_id, menu_name = content.split()
+        self.send_message("{}가 준비되었습니다. 카운터에서 수령해주세요.".format(menu_name), chat_id=sender_id)
+        message = Message(event).set_text('저희 가게를 이용하신 후 평가를 해주세요')\
+                                .add_quick_reply('평가하기','/feedback')
+        self.send_message(message, chat_id=sender_id)
+        # chat_id=sender_id : 요청을 보낸 사람의 id, 고객에게 대답 전송
+        self.send_message('고객분께 음료 완료 알림을 전송했습니다.')
+
+    def send_feedback_request(self):
+        self.send_message('음료를 맛있게 즐기셧다면 평가를 해주세요')
+        data = self.get_user_data()
+        data['wait_feedback'] = True
+        self.set_user_data(data)
+
+    def send_feedback(self, content, event):
+        chat_id = self.get_project_data().get('chat_id')
+        self.send_message('고객의 평가 메세지입니다\n{}'.format(content), chat_id=chat_id)
+
+        message = Message(event).set_text('평가해져서 감사합니다')\
+                                .add_quick_reply('메뉴보기')
+        self.send_message(message)
+        data = self.get_user_data()
+        data['wait_feedback'] = False
+        self.set_user_data(data)
 
